4bc884f046d8fda75fa59f4a8a42353090f696a0
codekraft-studio <info@codekraft.it>
2015-10-15 20:47:10 +0200

    some optimizations - enabled the renderHtml attribute

--- dist/content-editable.directive.js
+++ dist/content-editable.directive.js
@@ -2,9 +2,9 @@ angular.module('app')
 
 .directive('contentEditable', contentEditable)
 
-contentEditable.$inject = ['$log','$sce','$window']
+contentEditable.$inject = ['$log','$sce','$compile','$window']
 
-function contentEditable($log,$sce,$window) {
+function contentEditable($log,$sce,$compile,$window) {
 
   var directive = {
     restrict: 'A',
@@ -24,14 +24,15 @@ function contentEditable($log,$sce,$window) {
     }
 
     // if model is invalid or null
+    // set his value to elem content
     if( !scope.ngModel ) {
       ngModel.$setViewValue(elem.html())
     }
 
     // render with:
-    // model value or elem value
+    // model view value or elem value
     ngModel.$render = function() {
-      elem.html(ngModel.$viewValue || elem.html() )
+      elem.html( ngModel.$viewValue || elem.html() )
     }
 
     // make editable and focus
@@ -43,37 +44,81 @@ function contentEditable($log,$sce,$window) {
 
     // focus function
     elem.bind('focus', function(e) {
+
+      // set validity to true
+      // this is for custom behaviours
+      ngModel.$valid = true
+
       // select all on focus
       if( attrs.focusSelect ) {
         var range = $window.document.createRange()
         range.selectNodeContents(elem[0])
         $window.getSelection().addRange(range)
       }
+
+      // if render-html is enabled convert to plaintext
+      // in order to modify html tags
+      if( attrs.renderHtml ) {
+        var content = elem.html()
+        elem[0].textContent = content
+      }
+
     })
 
     // make not editable and lose focus
     elem.bind('blur', function() {
+
+      var html;
+
       // remove attribute
       elem.attr('contenteditable', 'false')
-      // get text content
-      var html = elem.html().replace(/<div>/g, '').replace(/&nbsp;/g, ' ').replace(/<\/div>/g, '');
-      // if modified and callback is defined
+
+      // if render html attribute is enabled
+      if( attrs.renderHtml && ngModel.$valid ) {
+        // get plain text html (with html tags)
+        // (replace blank spaces)
+        html = elem[0].textContent.replace(/\u00a0/g, " ")
+        // update elem html value
+        elem.html(html)
+
+      } else {
+        // clear text content
+        html = elem.html().replace(/<div>/g, '').replace(/&nbsp;/g, ' ').replace(/<\/div>/g, '');
+      }
+
+      // if element is different from his model
+      // and a callback isdefined run it
       if( html != ngModel.$modelValue && scope.editCallback ) {
-        // pass to callback: current text and element
+        // pass to callback values: current text and element
         scope.editCallback(html, elem)
       }
+
+      // set ngModel view value
       ngModel.$setViewValue(html)
     })
 
     // bind esc and enter keys
     elem.bind('keydown', function(e) {
+
+      // tab key: trigger blur event
+      if(e.which == 9) {
+        return elem[0].blur()
+      }
+
       // esc key: rollback and blur
       if(e.which == 27) {
         ngModel.$rollbackViewValue()
+        // set pristine state
+        // (alter renderHtml behaviours)
+        ngModel.$valid = false
         return elem[0].blur()
       }
+
       // enter key: simple blur
-      if(e.which == 13 && attrs.singleLine) { return elem[0].blur() }
+      if(e.which == 13 && attrs.singleLine) {
+        return elem[0].blur()
+      }
+
     })
 
   }
