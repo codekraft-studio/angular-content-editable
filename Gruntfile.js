0c8c98ed061f79612d6c0909c44159ea0df2b915
codekraft-studio <info@codekraft.it>
2016-05-25 23:50:21 +0200

    concat script

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,31 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: ['src/<%= pkg.name %>.module.js', 'src/<%= pkg.name %>.*.js'],
+        dest: 'dist/<%= pkg.name %>.js',
+      }
+    },
+
     uglify: {
-      options: { mangle: false },
+      options: {
+        mangle: false
+      },
       dist: {
         files: {
-          'dist/content-editable.directive.min.js': ['dist/content-editable.directive.js']
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
         }
       }
     }
 
   })
 
-  grunt.registerTask('default', ['uglify:dist'])
+  grunt.registerTask('default', ['concat:dist', 'uglify:dist'])
+
+  grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-contrib-uglify')
+
 }
